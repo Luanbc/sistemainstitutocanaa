b9c6c94c2f1fa7781542ab27bb71f5e57b853a49
Luan Costa <luan.costa@cest.edu.br>
2026-04-03 15:15:29 -0300

    fix: restore official supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://pckpfawvefcilatfwmqz.supabase.co';
-
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBja3BmYXd2ZWZjaWxhdGZ3bXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MjM3NzksImV4cCI6MjA4OTE5OTc3OX0.Z7l3MafVU7oE7Ifqh4JCT-CQIaslOrAJwQlbEGl-21M';
-
-
+const SUPABASE_URL = 'https://grmilcjrncnwdggfwosg.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdybWlsY2pybmNud2RnZ2Z3b3NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NTU4MjQsImV4cCI6MjA4OTUzMTgyNH0.AS3h0OMG8nmiuPnn1Beq6qybiHtMu7g97jHa9GWKHGs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
